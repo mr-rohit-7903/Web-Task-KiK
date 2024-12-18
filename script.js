da1843f9c96e6f8b833ed12841305e5e776be0ea
Rohit Bej <145686961+mr-rohit-7903@users.noreply.github.com>
2024-12-18 07:24:44 +0530

    Basic website

---
+++ script.js
@@ -0,0 +1,100 @@
+const apiKey = '13304bfd2fab4923936123123241512'; // WeatherAPI key
+const apiKey2 = '0qV2MDR1yvimMC5C0sqHOSqPI44U8q6D9WX6PIuBeDrAkzn0vMsL2J2n';  // Pexels API key
+
+let city = 'London';
+
+// Function to update Weather data
+function updateWeather(){
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`)
+        .then(response => response.json())
+        .then(data => {
+        
+        const name = data.location.name // Name of city
+        const tempC = data.current.temp_c; // Temperature in °C
+        const des = data.current.condition.text // Weather description
+        const humidity = data.current.humidity; // Humidity percentage
+        const windSpeed = data.current.wind_mph; // Wind speed in mph
+        const WeatherImage = {
+            "Images": {
+                "Sunny": "./images/clear.png",
+                "Partly cloudy": "images/clouds.png",
+                "Cloudy": "images/clouds.png",
+                "Overcast": "images/clouds.png",
+                "Mist": "images/mist.png",
+                "Light rain": "images/rain.png",
+                "Moderate rain": "images/rain.png",
+                "Heavy rain": "images/rain.png",
+                "Light snow": "images/snow.png",
+                "Moderate snow": "images/snow.png",
+                "Heavy snow": "images/snow.png",
+                "Patchy drizzle": "images/drizzle.png",
+                "Thunderstorm": "images/rain.png"
+            }
+        };
+        const weatherImg = WeatherImage.Images[des];
+
+        document.querySelector(".cityName").textContent = name;
+        document.querySelector(".temperature").innerHTML = `${tempC} °C`;
+        document.querySelector(".description").innerHTML = des;
+        document.querySelector(".humidity").innerHTML = `Humidity: ${humidity}%`;
+        document.querySelector(".windSpeed").innerHTML = `Wind: ${windSpeed} m/h`;
+        document.querySelector(".weatherImg").src = weatherImg;
+
+        console.log(`Temperature: ${tempC}°C`);
+        console.log(`Humidity: ${humidity}%`);
+        console.log(`Wind Speed: ${windSpeed} mph`);
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+// Function to add Background Image 
+function addBgImg(){
+    fetch(`https://api.pexels.com/v1/search?query=${city}`, {
+        headers: {
+            Authorization: apiKey2
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.photos && data.photos.length > 0) {
+            const randomIndex = Math.floor(Math.random() * data.photos.length);
+            const imageUrl = data.photos[randomIndex].src.large2x;
+            console.log(`Image URL: ${imageUrl}`);
+            bg = document.querySelector(".main")
+            bg.style.backgroundImage = `url(${imageUrl})`
+        } else {
+            console.error('No photos found for the given query.');
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+// Event listener to the search icon
+const search = document.querySelector("#search");
+const submit = document.querySelector(".searchIcon");
+submit.addEventListener("click", () => {
+    const enteredCity = search.value.trim();
+    if (enteredCity) {
+        city = enteredCity
+        updateWeather();
+        addBgImg();
+        search.value = "";
+    } else {
+        alert("Please enter a city name.");
+    }
+});
+
+// Toggle button 
+document.getElementById('myToggle').addEventListener('change', function () {
+    const tempElement = document.querySelector(".temperature");
+    const tempText = tempElement.innerHTML;
+    if (this.checked) {
+        const tempC = parseFloat(tempText.replace("°C", ""));
+        const tempF = (tempC * 9) / 5 + 32;
+        tempElement.innerHTML = `${tempF} °F`;
+    } else {
+        const tempF = parseFloat(tempText.replace("°F", ""));
+        const tempC = (tempF - 32) * 5 / 9;
+        tempElement.innerHTML = `${tempC} °C`;
+    }
+});
